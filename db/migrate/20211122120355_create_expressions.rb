3f737435c655b032851bb9dacc0d6ce2d2f73399
Mai Mouri <sfyitan@gmail.com>
2021-11-23 11:02:20 +0900

    sorry spelling miss

--- db/migrate/20211122120355_create_expressions.rb
+++ db/migrate/20211122120355_create_expressions.rb
@@ -3,7 +3,7 @@ class CreateExpressions < ActiveRecord::Migration[6.1]
     create_table :expressions do |t|
       t.references :participant, null: false, foreign_key: true
       t.integer :engagement_score, default: 0
-      t.decimal :confident, precision: 20, scale: 19
+      t.decimal :confidence, precision: 20, scale: 19
       t.string :emotion
 
       t.timestamps
