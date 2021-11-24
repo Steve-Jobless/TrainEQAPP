8b02fb14818ee2d5982c8d5314ae4957663eb213
Galym Parpiyev <parpiyev.galym@protonmail.com>
2021-11-24 12:14:25 +0900

    screen shot and analayze are working on a picture

--- chrome-extension/monitoring.js
+++ chrome-extension/monitoring.js
@@ -1,8 +1,34 @@
-const input = document.getElementById('myImg')
+const screenShot = document.getElementsByTagName('img')[0]
 console.log("Hello!")
+console.log(screenShot)
 
 
 
+
+
+const takeScreenShot = () => {
+  return html2canvas(document.querySelector("img"))
+}
+
+
+const startMonitoring = (screenShot) => {
+  console.log("The start monitor is being called")
+  //create a meeting
+
+  //every 30 seconds:
+  setInterval(async () => {
+    console.log("inside of the interval")
+    //take screenshot of the canvas
+    const testShot = await takeScreenShot()
+    //feed the screenshot into the emotion-detector
+    console.log(testShot)
+    const emotions = analyzeEmotions(testShot)
+    //output sent to the api at the back
+    // logResultsToBE(emotions)
+    //display the feedback
+    // displayResults(emotions)
+  }, 10000)
+}
 
 //load models
 
@@ -12,36 +38,21 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
   faceapi.nets.faceExpressionNet.loadFromUri('./models'),
   console.log("loading models")
-]).then(startMonitoring)
+]).then(startMonitoring(screenShot))
 
 
-const startMonitoring = () => {
-  //create a meeting
 
-  //every 30 seconds:
-  setInterval( () => {
-    //take screenshot of the canvas
-    const screenShot = takeScreenShot()
-    //feed the screenshot into the emotion-detector
-    const emotions = analyzeEmotions(screenShot)
-    //output sent to the api at the back
-    logResultsToBE(emotions)
-    //display the feedback
-    displayResults(emotions)
-  }, 30000)
-}
-
-const takeScreenShot = () => {}
 
 const analyzeEmotions = (screenShot) => {
     setTimeout(async () => {
       const detections = await faceapi
-        .detectAllFaces(input, new faceapi.TinyFaceDetectorOptions())
+        .detectAllFaces(screenShot, new faceapi.TinyFaceDetectorOptions())
         .withFaceLandmarks()
         .withFaceExpressions()
       console.log(detections)
         , 10000
     })
+    console.log("inside of analyze")
 }
 
 const logResultsToBE = (emotions) => {}
