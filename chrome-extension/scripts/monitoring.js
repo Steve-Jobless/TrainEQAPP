eb279f887eff9ce6e78279ab086b1bf011fb5056
Galym Parpiyev <parpiyev.galym@protonmail.com>
2021-11-24 15:39:51 +0900

    the face api is working on videos

---
+++ chrome-extension/scripts/monitoring.js
@@ -0,0 +1,51 @@
+// const screenShot = document.getElementsByTagName('img')[0]
+console.log("Hello!")
+// console.log(screenShot)
+
+let port
+setTimeout(() => {
+  port = chrome.runtime.connect({ name: "emotionDetector" });
+  port.onMessage.addListener(function (msg) {
+    console.log(msg)
+
+  });
+}, 1000);
+
+
+const takeScreenShot = () => {
+  // return html2canvas(document.querySelector("#test"))
+  var canvas = document.createElement('canvas');
+  canvas.width = 640;
+  canvas.height = 480;
+  var ctx = canvas.getContext('2d');
+  const video = document.querySelectorAll("video")[0]
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  return canvas
+}
+
+
+
+
+const startMonitoring =  () => {
+  console.log("The start monitor is being called")
+  //create a meeting
+
+  //every 30 seconds:
+  setInterval(async () => {
+    console.log("inside of the interval")
+    //take screenshot of the canvas
+    const screenShot = await takeScreenShot()
+    //feed the screenshot into the emotion-detector
+    port.postMessage({ screenShot: screenShot.toDataURL() });
+
+    //output sent to the api at the back
+
+    //display the feedback
+    // displayResults(emotions)
+  }, 3000)
+}
+
+
+const displayResults = (emotions) => {}
+
+startMonitoring()
