d9a832742addbb409e9720f5c3cb6b9beed016c9
Mai Mouri <sfyitan@gmail.com>
2021-11-25 12:24:09 +0900

    dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,24 @@ class UsersController < ApplicationController
     @meetings = Meeting.all.where(user_id: current_user)
     # @bookings_as_owner = current_user.bookings_as_owner
     @expressions = Expression.all.where(user_id: current_user)
+    # @participant = Participant.find(1)
+    # @happy = Expression.all.where(participant_id:1).where(emotion:"happy").count
+    # @sad = Expression.all.where(participant_id:1).where(emotion:"sad").count
+    # @neutral = Expression.all.where(participant_id:1).where(emotion:"neutral").count
+    # @angry = Expression.all.where(participant_id:1).where(emotion:"angry").count
+    # @disgusted = Expression.all.where(participant_id:1).where(emotion:"disgusted").count
+    # @suprised = Expression.all.where(participant_id:1).where(emotion:"suprised").count
+    # @fearful = Expression.all.where(participant_id:1).where(emotion:"fearful").count
+    # @emotions = [ @happy, @sad, @neutral, @angry, @disgusted, @surprised, @fearful]
+    @emotions = [
+      Expression.all.where(participant_id:1).where(emotion:"happy").count,
+      Expression.all.where(participant_id:1).where(emotion:"sad").count,
+      Expression.all.where(participant_id:1).where(emotion:"neutral").count,
+      Expression.all.where(participant_id:1).where(emotion:"angry").count,
+      Expression.all.where(participant_id:1).where(emotion:"disgusted").count,
+      Expression.all.where(participant_id:1).where(emotion:"surprised").count,
+      Expression.all.where(participant_id:1).where(emotion:"fearful").count
+    ]
+
   end
 end
