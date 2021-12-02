1429e3af720bbb5b858fad576a3afc380fbdbeb4
Mai Mouri <sfyitan@gmail.com>
2021-12-02 14:24:27 +0900

    Test commit repo using ruby 3.0.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     authorize current_user
     @meetings = Meeting.all.where(user_id: current_user)
     @meeting = Meeting.all.where(user_id: current_user).last
+    # @meeting = Meeting.find(81)
     @participants = Participant.all.where(meeting_id: @meeting.id)
     @expressions = Expression.all.where(user_id: current_user)
     @disengaged = ["sad", "angry", "disgusted", "fearful"]
