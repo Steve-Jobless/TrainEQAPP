027d3dbb8d19fe839711f70e7c18b2b87d30764d
Mai Mouri <sfyitan@gmail.com>
2021-12-02 18:28:30 +0900

    scores updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
     authorize current_user
     @meetings = Meeting.all.where(user_id: current_user)
     @meeting = Meeting.all.where(user_id: current_user).last
-    # @meeting = Meeting.find(81)
     @participants = Participant.all.where(meeting_id: @meeting.id)
     @expressions = Expression.all.where(user_id: current_user)
     @disengaged = ["sad", "angry", "disgusted", "fearful"]
     @engaged = ["happy", "neutral", "surprised"]
     @all_emotions = []
     comparison
+    scores
   end
 
   def comparison
@@ -36,5 +36,30 @@ class UsersController < ApplicationController
           @total_engages = @disengaged_count + @engaged_count
           @five_meetings_results << (@engaged_count* 100 / @total_engages).to_i
       end
+  end
+
+  def scores
+    @meetings = Meeting.all.where(user_id: current_user)
+    @all_expressions_score = []
+    @total_disengaged_count = 0
+    @total_engaged_count = 0
+    @number_of_meetings = Meeting.all.where(user_id: current_user).count
+    @meetings.each do |meeting|
+      @all_participants = Participant.all.where(meeting_id: meeting.id)
+      if !@all_participants.nil?
+        @all_participants.each do |participant|
+          @all_expressions = Expression.all.where(participant_id: participant.id)
+          @all_expressions.each do |expression|
+            @all_expressions_score << expression.emotion
+              if @disengaged.include?(expression.emotion)
+                @total_disengaged_count += 1
+              else
+                @total_engaged_count += 1
+              end
+          end
+        end
+      end
+    end
+    @total_happys = @all_expressions_score.count('happy')
   end
 end
